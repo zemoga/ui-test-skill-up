bce7e6e944aa3522486ab4092061bb39ce674e86
Leslie Fernandez <leslie.fernandez@zemoga.com>
2022-07-06 14:51:11 -0500

    Add basic layout of the project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import HomeView from "./views/HomeView.vue";
+</script>
+
+<template>
+  <main>
+    <HomeView></HomeView>
+  </main>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+#app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+}
+</style>
