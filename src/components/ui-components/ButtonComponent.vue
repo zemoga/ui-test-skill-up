6fe7e8b0570273ece50f3e3113fb0a96d2e27a06
Leslie Fernandez <leslie.fernandez@zemoga.com>
2022-07-06 15:34:10 -0500

    Add the general styles of all buttons on mobile

--- src/components/ui-components/ButtonComponent.vue
+++ src/components/ui-components/ButtonComponent.vue
@@ -8,6 +8,66 @@
 
 <style>
 .btn {
+  height: 4rem;
   border-radius: 0.8rem;
+  border: none;
+  font-size: 1.6rem;
+}
+
+.primary {
+  width: 11.8rem;
+  background: #000;
+  color: #fff;
+}
+
+.secondary {
+  width: 17.3rem;
+  background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 0.72),
+      rgba(255, 255, 255, 0.72)
+    ),
+    #000000;
+  color: #fff;
+}
+
+.cta {
+  width: 9.9rem;
+  background: #fff;
+  border: 0.2rem solid #000;
+  color: #000;
+}
+
+.forward {
+  width: 4rem;
+  background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 0.96),
+      rgba(255, 255, 255, 0.96)
+    ),
+    #000000;
+  border-radius: 4rem;
+}
+
+.back {
+  width: 4rem;
+  background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 0.72),
+      rgba(255, 255, 255, 0.72)
+    ),
+    #000000;
+  border-radius: 4rem;
+}
+
+.login {
+  width: 7.9rem;
+  background: linear-gradient(
+      0deg,
+      rgba(255, 255, 255, 0.72),
+      rgba(255, 255, 255, 0.72)
+    ),
+    #000000;
+  color: #fff;
 }
 </style>
