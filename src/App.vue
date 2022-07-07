b4e59ed164702f14cee5b17e1ee8dd58a4c62791
Leslie Fernandez <leslie.fernandez@zemoga.com>
2022-07-07 10:22:35 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup>
+import BookCard from "@/components/BookCard.vue";
+</script>
+
+<template>
+  <BookCard></BookCard>
+</template>
---
+++ src/components/BookCard.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="book">
+    <h3 class="book__title"></h3>
+    <h4 class="book__author"></h4>
+    <p class="book__members"></p>
+    <button>Vote</button>
+  </div>
+</template>
