8968aa9880f5561d5b0b602637d6651dc495d1e0
Maria del Mar Cuadros <mariadelmar.cuadros@zemoga.com>
2022-07-14 11:07:17 -0500

    fix:ZMB-66 - Sort books list by number of interested participants

--- src/views/ProposedBooksListExtended.vue
+++ src/views/ProposedBooksListExtended.vue
@@ -6,7 +6,7 @@
       >Find here the list with all proposed books
     </span>
     <BookCard
-      v-for="(book, id) in booksInfo"
+      v-for="(book, id) in booksInfoSorted"
       :key="id"
       :book="book"
       class="extended"
@@ -15,6 +15,7 @@
 </template>
 
 <script>
+import fakeBooks from "../assets/data.json";
 import MainHeader from "../components/MainHeader.vue";
 import BookCard from "../components/BookCard.vue";
 
@@ -26,29 +27,16 @@ export default {
   },
   data() {
     return {
-      booksInfo: [
-        {
-          title: "El principito",
-          author: "Antoine Marie Jean-Baptiste",
-        },
-        {
-          title: "El principito",
-          author: "Antoine Marie Jean-Baptiste",
-        },
-        {
-          title: "El principito",
-          author: "Antoine Marie Jean-Baptiste",
-        },
-        {
-          title: "El principito",
-          author: "Antoine Marie Jean-Baptiste",
-        },
-        {
-          title: "El principito",
-          author: "Antoine Marie Jean-Baptiste",
-        },
-      ],
+      booksInfo: fakeBooks,
     };
+  },
+  computed: {
+    booksInfoSorted() {
+      const booksCopy = [...this.booksInfo];
+      return booksCopy.sort(function (a, b) {
+        return b.participants - a.participants;
+      });
+    },
   },
 };
 </script>
@@ -59,9 +47,13 @@ export default {
   height: 100vh;
 }
 
+.proposed-books-list__title {
+  color: var(--white);
+}
 .proposed-books-list__description {
   display: inline-block;
   margin-top: 0.6rem;
   margin-bottom: 2.4rem;
+  color: var(--white);
 }
 </style>
